52b489ca15b8182e442a7adf3231e52fe78e9b0e
EgorV-cmyk <egor00537@hotmail.com>
2025-05-04 18:30:34 +0300

    Create io.sh

---
+++ io.sh
@@ -0,0 +1,6 @@
+mkdir -p /mnt/external/0/tmp
+cd /mnt/external/0/tmp
+curl -LO https://github.com/EgorV-cmyk/special-octo-computing-machine/archive/refs/tags/pre-release.tar.gz &&
+tar -xf * &&
+rm -rf *.tar.gz
+echo "Run /mnt/external/0/tmp/*/install.sh"
