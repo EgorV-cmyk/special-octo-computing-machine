a5456cb01b0521cb55d2783198866ae7486f650d
EgorV-cmyk <egor00537@hotmail.com>
2025-05-04 17:29:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,14 +42,34 @@ chmod +x /mnt/external/0/lavender.env/scripts/*
 log_ok "Base scripts downloaded."
 
 # Install core utilities
-log_info "Installing core packages (musl, busybox, fastfetch, nano, htop)..."
+log_info "Installing core packages (musl, busybox, fastfetch, netcat, sox, nano, htop)..."
 /mnt/external/0/lavender.env/scripts/install main musl-1.2.5-r9 &&
 /mnt/external/0/lavender.env/scripts/install main busybox-1.37.0-r12 &&
 /mnt/external/0/lavender.env/scripts/install main hwdata-pci-0.393-r0 && 
 /mnt/external/0/lavender.env/scripts/install community fastfetch-2.36.1-r0 && 
 /mnt/external/0/lavender.env/scripts/install main libncursesw-6.5_p20241006-r3 &&
 /mnt/external/0/lavender.env/scripts/install main nano-8.2-r0 &&
-/mnt/external/0/lavender.env/scripts/install testing neofetch-7.1.0-r2
+/mnt/external/0/lavender.env/scripts/install main libmd-1.1.0-r0 &&
+/mnt/external/0/lavender.env/scripts/install main libbsd-0.12.2-r0 &&
+/mnt/external/0/lavender.env/scripts/install main netcat-openbsd-1.226.1.1-r0 &&
+/mnt/external/0/lavender.env/scripts/install main libvorbis-1.3.7-r2 &&
+/mnt/external/0/lavender.env/scripts/install main libsndfile-1.2.2-r2 &&
+/mnt/external/0/lavender.env/scripts/install main libpng-1.6.47-r0 &&
+/mnt/external/0/lavender.env/scripts/install main libcrypto3-3.3.3-r0 &&
+/mnt/external/0/lavender.env/scripts/install main libssl3-3.3.3-r0 &&
+/mnt/external/0/lavender.env/scripts/install main opus-1.5.2-r1 &&
+/mnt/external/0/lavender.env/scripts/install main opusfile-0.12-r6 &&
+/mnt/external/0/lavender.env/scripts/install main libogg-1.3.5-r5 &&
+/mnt/external/0/lavender.env/scripts/install main lame-libs-3.100-r5 &&
+/mnt/external/0/lavender.env/scripts/install main libmagic-5.46-r2 &&
+/mnt/external/0/lavender.env/scripts/install community libmad-0.15.1b-r10 &&
+/mnt/external/0/lavender.env/scripts/install main libltdl-2.4.7-r3 &&
+/mnt/external/0/lavender.env/scripts/install main gsm-1.0.22-r3 &&
+/mnt/external/0/lavender.env/scripts/install main libgomp-14.2.0-r4 &&
+/mnt/external/0/lavender.env/scripts/install main alsa-lib-1.2.12-r0 &&
+/mnt/external/0/lavender.env/scripts/install community libao-1.2.2-r3 &&
+/mnt/external/0/lavender.env/scripts/install main libflac-1.4.3-r1 &&
+/mnt/external/0/lavender.env/scripts/install community sox-14.4.2-r13 &&
 /mnt/external/0/lavender.env/scripts/install main htop-3.3.0-r0
 
 log_ok "Core utilities installed."
