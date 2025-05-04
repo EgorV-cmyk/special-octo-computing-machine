d361dbf7860815cbbac587f66a60949936e72d20
EgorV-cmyk <egor00537@hotmail.com>
2025-05-04 18:53:54 +0300

    Delete ia.sh

--- ia.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /mnt/external/0/tmp
-cd /mnt/external/0/tmp
-curl -LO https://github.com/EgorV-cmyk/special-octo-computing-machine/archive/refs/tags/pre-release.tar.gz &&
-tar -xf * &&
-rm -rf *.tar.gz
-chmod 777 /mnt/external/0/tmp/*/install.sh
-echo "Run /mnt/external/0/tmp/*/install.sh"
