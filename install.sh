35f240e00ac2945ce17e3bbae2fb7169298f3f67
EgorV-cmyk <egor00537@hotmail.com>
2025-05-04 11:37:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ for DIR in \
   /mnt/external/0/usr/share \
   /mnt/external/0/etc \
   /mnt/external/0/etc/home \
-  /mnt/external/0/etc/home/.config/fish \
   /mnt/external/0/lavender.env \
   /mnt/external/0/lavender.env/tmp \
   /mnt/external/0/lavender.env/scripts \
@@ -46,15 +45,7 @@ log_ok "Base scripts downloaded."
 log_info "Installing core packages (musl, busybox, fish, nano, htop)..."
 /mnt/external/0/lavender.env/scripts/install musl-1.2.5-r9 &&
 /mnt/external/0/lavender.env/scripts/install busybox-1.37.0-r12 &&
-/mnt/external/0/lavender.env/scripts/install ncurses-terminfo-6.5_p20241006-r3 &&
-/mnt/external/0/lavender.env/scripts/install ncurses-terminfo-base-6.5_p20241006-r3 &&
-/mnt/external/0/lavender.env/scripts/install libintl-0.22.5-r0 &&
-/mnt/external/0/lavender.env/scripts/install libgcc-14.2.0-r4 &&
-/mnt/external/0/lavender.env/scripts/install fish-3.7.1-r0 &&
 /mnt/external/0/lavender.env/scripts/install libncursesw-6.5_p20241006-r3 &&
-/mnt/external/0/lavender.env/scripts/install ncurses-6.5_p20241006-r3 &&
-/mnt/external/0/lavender.env/scripts/install libpcre2-32-10.43-r0 &&
-/mnt/external/0/lavender.env/scripts/install libstdc++-14.2.0-r4 && 
 /mnt/external/0/lavender.env/scripts/install nano-8.2-r0 &&
 /mnt/external/0/lavender.env/scripts/install htop-3.3.0-r0
 
@@ -62,9 +53,9 @@ log_ok "Core utilities installed."
 
 # Download fish config and startup script
 log_info "Fetching config and startup files..."
-curl -o /mnt/external/0/etc/home/.config/fish/config.fish https://raw.githubusercontent.com/EgorV-cmyk/special-octo-computing-machine/refs/heads/main/fish-conf
-curl -o /mnt/external/0/start-fish https://raw.githubusercontent.com/EgorV-cmyk/special-octo-computing-machine/refs/heads/main/start-fish
-chmod +x /mnt/external/0/start-fish
+curl -o /mnt/external/0/lavender.env/bashrc https://raw.githubusercontent.com/EgorV-cmyk/special-octo-computing-machine/refs/heads/main/bash-conf
+curl -o /mnt/external/0/start-bash https://raw.githubusercontent.com/EgorV-cmyk/special-octo-computing-machine/refs/heads/main/start-bash
+chmod +x /mnt/external/0/start-bash
 log_ok "Configuration files ready."
 
 # Final notes
@@ -73,5 +64,5 @@ printf "${INFO} Inside fish session, install packages with: ${GREEN}install name
 # printf "${INFO} To remove packages: ${GREEN}remove name-version${RESET}\n"
 printf "${INFO} Example: ${GREEN}nano-8.2-r0${RESET}\n"
 printf "${INFO} Use: ${GREEN}run <program>${RESET} to start installed programs.\n"
-printf "\n${INFO} To launch Lavender environment, run: ${GREEN}/mnt/external/0/start-fish${RESET}\n"
-/mnt/external/0/start-fish
+printf "\n${INFO} To launch Lavender environment, run: ${GREEN}/mnt/external/0/start-bash${RESET}\n"
+/mnt/external/0/start-bash
